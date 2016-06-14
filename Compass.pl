79bc03a53257d4f6a34514d3e8458a16a4802a62
John <iorekbyre@gmail.com>
2016-06-14 10:04:58 +0100

    Compass program in Perl
    This is my first attempt at coding in Perl. I created a very simple program that determines which 2 cardinal points a person is in between based on the degree bearing they enter. I got the idea from Dr Isac Artzi's outstanding C course on Lynda (C Essential Training).
    
    If anyone could suggest improvements or pointers please fire away. It's my first project in Perl. 
    
    P.S. Sorry about the weird tab space I'm using Padre for the first time.

---
+++ Compass.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+
+print "Enter the degree bearing you are facing\n";
+my $direction = <>;
+#First if statement tests if value entered is a number or blank, nested if statement tests which direction person is facing
+#I used regex /^\ is the start of the input, \d will only accept values that are 0-9, $/ is the end of the input
+if ( ($direction =~ /^\d+$/) && (0 <= $direction) && ($direction <= 360)) 
+	{
+	 if ($direction <= 90)
+			{print "You are facing between North and East \n";}		
+	 elsif ($direction <= 180)
+			{print "You are facing between East and South \n";}
+	 elsif ($direction <= 270)
+			{print "You are facing between South and West \n";}
+	 elsif ($direction <= 360)
+			{print "You are facing between West and North \n";}				}
+else
+	{print "You need to enter a value between 0 and 360 \n";}
